4216ca38e62c9c88b0a3e719bc43a66886ec41d0
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2026-06-09 19:57:02 +0200

    Redirect authenticated users from landing

--- ineat-frontend/src/routes/index.tsx
+++ ineat-frontend/src/routes/index.tsx
@@ -1,7 +1,17 @@
-import { createFileRoute, Link } from '@tanstack/react-router';
+import { createFileRoute, Link, redirect } from '@tanstack/react-router';
 import LandingPage from '@/pages/landing/LandingPage';
+import { isAuthenticated } from '@/utils/auth';
 
 export const Route = createFileRoute('/')({
+	beforeLoad: async () => {
+		const userIsAuthenticated = await isAuthenticated();
+
+		if (userIsAuthenticated) {
+			throw redirect({ to: '/app' });
+		}
+
+		return null;
+	},
 	component: LandingPage,
 
 	errorComponent: ({ error }) => {
